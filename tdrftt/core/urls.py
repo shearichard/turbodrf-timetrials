c7cc63d15371963e17c5e6d6c65f9b17abb9c7f2
Richard Shea <rshea@thecubagroup.com>
2025-12-06 13:13:03 +1300

    Initiated use of Swagger. Doing so has highlighted that some of the routing is now what it should be as there are duplicate paths exposed to the same resources, but this can be fixed in a later commit.

--- tdrftt/core/urls.py
+++ tdrftt/core/urls.py
@@ -18,14 +18,34 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from turbodrf import urls as turbodrf_urls
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 admin.site.site_title = "TurboDRF TimeTrials Site Admin (DEV)"
 admin.site.site_header = "TurboDRF TimeTrials Admin"
 admin.site.index_title = "Site Administration"
 
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Turbo-DRF API",
+        default_version='v1',
+        description="Turbo-DRF Timetrial API",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@yourapi.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('turbodrftimetrial/', include('turbodrftimetrial.urls')),
     path('api-x/', include(turbodrf_urls)),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
